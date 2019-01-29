c70df2e14bc5c2e1af8a318f0a3d5f277943d31b
S. M. Fahim Rafi Siddiqui <s.m.fahim.ju@gmail.com>
2019-01-30 00:48:15 +0600

    Added assignment 4

---
+++ assignment_4/4_1.php
@@ -0,0 +1,37 @@
+
+
+<?php  
+//run this code on console
+
+//this program is valide for only odd number of lines
+
+
+$line=21;
+
+$H= ($line/2)+1;
+
+for($i=0;$i<=$H;$i++)
+{  
+for($k=$H;$k>=$i;$k--)
+{  
+echo "  ";  
+}  
+for($j=1;$j<=$i;$j++)
+{  
+echo " *  ";  
+}  
+echo "\n";  
+}  
+for($i=($H-1);$i>=1;$i--)
+{  
+for($k=$H;$k>=$i;$k--)
+{  
+echo "  ";  
+}  
+for($j=1;$j<=$i;$j++)
+{  
+echo " *  ";  
+}  
+echo "\n";  
+}  
+?>  
